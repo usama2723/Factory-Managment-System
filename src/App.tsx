e293ffd25c51a717118c5b2d94f2d076f9f7c42d
usama2723 <sammirza201@gmail.com>
2025-03-23 16:28:34 +0500

    fixed routr issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,13 @@ import Signup from './pages/signup';
 import Login from './pages/login';
 import Dashboard from './pages/dashboard';
 
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} /> 
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
